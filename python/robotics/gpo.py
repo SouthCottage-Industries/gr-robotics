8a975266712f7f171717b7cc64fc73921d21f297
Rachel Kirkwood <rachelsroof@gmail.com>
2025-03-18 17:39:14 -0400

    Modified gpo block and created a test flowgraph that blinks an LED using GPIO17
    GPIO17 is currently hardcoded as the pin number, but we want to pass as a parameter to the block.

--- python/robotics/gpo.py
+++ python/robotics/gpo.py
@@ -21,17 +21,21 @@ class gpo(gr.sync_block):
     def __init__(self, platform="pi3", gpio_pin=11):
         gr.sync_block.__init__(self,
             name="gpio",
-            in_sig=[numpy.bool, ],
+            in_sig=[numpy.float32, ],
             out_sig=None)
         gpop = gpio_pin
         GPIO.setmode(GPIO.BOARD)
-        GPIO.setup(gpop, GPIO.OUT)
-
+        GPIO.setup(11, GPIO.OUT)
+#        GPIO.output(gpop, GPIO.LOW)
 
     def work(self, input_items, output_items):
         in0 = input_items[0]
         
-        for x in in0:
-            GPIO.output(gpop, x != 0)
+#        for x in in0:
+#            GPIO.output(gpop, x != 0)
 
+        if any(in0) == 1:
+            GPIO.output(11, GPIO.HIGH)
+        else:
+            GPIO.output(11,GPIO.LOW)
         return len(input_items[0])
