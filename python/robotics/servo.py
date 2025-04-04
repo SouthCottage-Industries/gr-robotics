6308813351d3a41c39cbb708e673a1f471930808
Rachel Kirkwood <rachelsroof@gmail.com>
2025-04-04 13:24:18 -0400

    modified servo block to be driven by angle input. Currently not functioning, no change is seen on the servo motor when tested.

--- python/robotics/servo.py
+++ python/robotics/servo.py
@@ -11,27 +11,41 @@ import numpy
 import RPi.GPIO as GPIO # type: ignore
 from gnuradio import gr
 
-class gpo(gr.sync_block):
+class servo(gr.sync_block):
     """
     docstring for block gpo
     """
 
-    def __init__(self, platform="pi3", gpio_pin=11):
-    #def __init__(self, gpio_pin=11):
+    def __init__(self, gpio_pin=12, frequency=50):
         gr.sync_block.__init__(self,
-            name="gpio",
+            name="servo",
             in_sig=[numpy.int32, ],
             out_sig=None)
-        
         self.gpop = gpio_pin
+        self.freq = frequency
+        self.duty_cycle = 0.075
+        self.angle = 0
         GPIO.setmode(GPIO.BOARD)
         GPIO.setup(self.gpop, GPIO.OUT)
-
+        self.pwm = GPIO.PWM(self.gpop, self.freq)
+        self.pwm.start(self.duty_cycle)
+        
+    def change_frequency(self,new_freq):
+        self.freq = new_freq
+        
+    def set_angle(self, angle):
+        if angle > 180 or angle < 0:
+            return
+        self.duty_cycle = ((angle/180.0 * 2) + 0.5) / 20
 
     def work(self, input_items, output_items):
         in0 = input_items[0]
 
         for x in in0:
-            GPIO.output(self.gpop, x)
+            if x != self.angle:
+                self.angle = x
+                self.set_angle(self.angle)
+                self.pwm.ChangeDutyCycle(self.duty_cycle)
+                
 
         return len(input_items[0])
