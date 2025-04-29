47bd5a4c718fa9abe3798bc18cd275749d1ca154
lordFreddie <mark.southcott@southcottage-industries.com>
2025-04-28 22:05:50 -0400

    updated r2mc.py

--- examples/r2mc.py
+++ examples/r2mc.py
@@ -44,6 +44,7 @@ class r2mc(gr.top_block):
         self.blocks_sub_xx_0 = blocks.sub_ff(1)
         self.blocks_multiply_const_vxx_0 = blocks.multiply_const_ff((1-min_speed)/(max_range-min_range))
         self.blocks_add_const_vxx_0 = blocks.add_const_ff(min_speed)
+        self.analog_rail_ff_0 = analog.rail_ff(.25, 1)
         self.analog_const_source_x_0 = analog.sig_source_f(0, analog.GR_CONST_WAVE, 0, 0, min_range)
 
 
@@ -51,7 +52,8 @@ class r2mc(gr.top_block):
         # Connections
         ##################################################
         self.connect((self.analog_const_source_x_0, 0), (self.blocks_sub_xx_0, 1))
-        self.connect((self.blocks_add_const_vxx_0, 0), (self.robotics_gpo_pwm_0, 0))
+        self.connect((self.analog_rail_ff_0, 0), (self.robotics_gpo_pwm_0, 0))
+        self.connect((self.blocks_add_const_vxx_0, 0), (self.analog_rail_ff_0, 0))
         self.connect((self.blocks_multiply_const_vxx_0, 0), (self.blocks_add_const_vxx_0, 0))
         self.connect((self.blocks_sub_xx_0, 0), (self.blocks_multiply_const_vxx_0, 0))
         self.connect((self.robotics_ultrasonic_ranger_0, 0), (self.blocks_sub_xx_0, 0))
